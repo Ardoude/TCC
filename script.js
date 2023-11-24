29f6af23e24aa9e15cc6bf6d9d24ae4ebc4f7365
Eduardo <eduardopcosta01@gmail.com>
2023-11-24 16:17:32 -0300

    Layout + Pesquisa por nome

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector("#container");
+const searchInput = document.querySelector("#search");
+var dados;
 
 async function carrega(){
-    var dados;
 
     await fetch('dados.json')
     .then(response => response.json())
@@ -12,7 +13,6 @@ async function carrega(){
     dados.forEach(dado => {
         createCard(dado)
     });
-
 }
 
 const createCard = (dado) => {
@@ -40,5 +40,40 @@ const createCard = (dado) => {
     card.innerHTML = cardElement
     container.appendChild(card)
 }
+
+const search = (input) => {
+    input = input.toUpperCase()
+    let copiaDados = JSON.parse(JSON.stringify(dados));
+    let resultado = []
+    let aux
+
+    var novosDados = copiaDados.filter(function (entry) {
+        aux = entry && entry.nome && entry.nome.toUpperCase().indexOf(input)!==-1
+        
+        if(aux){
+            resultado.push(entry)
+        }
+    });
+
+    limpar() // Limpar dados já presentes
+
+    // Adicionar resultado da busca
+    resultado.forEach( dado =>{
+        createCard(dado)
+    })
+}
+
+const limpar = () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// Escutar o evento de change do input de search
+searchInput.addEventListener('change', () => {
+    // Recuperaro valor digitado no input de search
+    const searchValue = searchInput.value;
+    search(searchValue)
+})
 
 carrega()
